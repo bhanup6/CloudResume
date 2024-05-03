bbe3faa718c183da50553678be4d81d6d76a5828
BhanuPrakash <bhanup6@gmail.com>
2024-05-02 23:41:03 -0400

    modified the index.html and script.js with visitor count

--- js/scripts.js
+++ js/scripts.js
@@ -75,3 +75,12 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+const counter = document.querySelector(".counter-number");
+async function updateCounter() {
+    let response = await fetch(
+        "https://v7akbrstqlpdscuvepsgeuihsu0ohxku.lambda-url.us-east-1.on.aws/"
+    );
+    let data = await response.json();
+    counter.innerHTML = `Views: ${data}`;
+}
+updateCounter();
